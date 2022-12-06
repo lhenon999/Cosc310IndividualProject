4c698556d3848f858931bc5c73c6017178f55681
lhenon999 <leohenon@gmail.com>
2022-12-05 21:03:37 -0800

    removed inccorect import

--- components/InventoryPanel.jsx
+++ components/InventoryPanel.jsx
@@ -4,7 +4,6 @@ import React from "react";
 import styles from "/styles/InventoryPanel.module.css";
 import { useState, useEffect, useRef } from "react";
 import Google from ".//GoogleMaps.jsx";
-import AlertsPanel from "./AlertsPanel.jsx";
 
 
 export class Warehouse {
